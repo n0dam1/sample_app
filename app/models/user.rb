bc3c1f1134749f2998ec7fea408991854d59e8d5
Minoru Noda <noda@healthdataplatform.co.jp>
2018-07-05 13:59:53 +0900

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -11,5 +12,23 @@ class User < ApplicationRecord
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token # リメンバートークンをランダムに生成
+    update_attribute(:remember_digest, User.digest(remember_token)) # リメンバートークンをハッシュかましてDBのリメンバーダイジェストに保存
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token) # DBのリメンバーダイジェストとリメンバートークンを比較（ハッシュ値とローの比較だがis_password?がよしなにやってる
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
