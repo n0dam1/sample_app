d1377c83ae91999037ae34a2781749f67fc39962
Minoru Noda <noda@healthdataplatform.co.jp>
2018-07-06 11:43:23 +0900

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies.signed[:remember_token]) #ユーザーがいてクッキーにあるトークンがDBのリメンバーダイジェストと一致したら
+      if user && user.authenticated?(:remember, cookies.signed[:remember_token]) #ユーザーがいてクッキーにあるトークンがDBのリメンバーダイジェストと一致したら
         log_in user
         @current_user = user
       end
