b1f75c94ec3f95566163d0cedc50a2e4264c0c80
Minoru Noda <noda@healthdataplatform.co.jp>
2018-07-06 14:35:08 +0900

    Add password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,7 +14,7 @@ class PasswordResetsController < ApplicationController
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
     else
-      flash[:danger] = "Email address not found"
+      flash.now[:danger] = "Email address not found"
       render 'new'
     end
 
